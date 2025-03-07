a897fe9de0ddba68ded808716075b1b02e83d793
Danton Jair <dantonjairquispechuquimamani@gmail.com>
2025-03-07 18:56:41 -0500

    correccion de ortografia en el codigo de convertidor de grados

--- convertidor.py
+++ convertidor.py
@@ -1,3 +1,3 @@
-celcius = (float(input("ingrse un numeroen grados c")))
+celcius = (float(input("ingrese un numero en grados C")))
 fahrenheit = (celcius*9/5)+32
 print(f"{celcius} grados celcius en fahrenheit es {fahrenheit} ")
