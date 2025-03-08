44cccd8035644e632f8f50dfc05f2e867a4d82b7
Danton Jair <dantonjairquispechuquimamani@gmail.com>
2025-03-07 19:35:13 -0500

    codigo en python para hallar el perimetro de un rectangulo

---
+++ perimetro_de_rectangulo.py
@@ -0,0 +1,4 @@
+largo = (float(input("dime el largo de tu rectangulo ")))
+ancho = (float(input("dime el anho de tu rectangulo")))
+perimetro =  (largo + ancho)*2
+print(f"el perimetro de tu rectangulo es {perimetro}")
