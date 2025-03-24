d0f4053d55757b2ffff7684a8832f27c3d958ed9
Danton Jair <dantonjairquispechuquimamani@gmail.com>
2025-03-24 18:14:38 -0500

    nuevos ejersicios resueltos

---
+++ par_Impar.py
@@ -0,0 +1,5 @@
+numero = (int(input("ingrese un numero")))
+if numero % 2 == 1:
+    print("tu numero es inpar")
+else :
+    print(" tu numero es par")
---
+++ positivo_negativo.py
@@ -0,0 +1,7 @@
+numero = (int(input("dame un numero")))
+if       numero < 0:
+    print("tu numero es negativo")
+elif     numero > 0:
+    print("tu numero es positivo")
+else:     
+    print("tu numero es igual a cero")
